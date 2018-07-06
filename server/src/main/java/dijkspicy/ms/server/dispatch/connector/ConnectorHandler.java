a654ad86a0f4101bc12c54541aea6986962f6a33
dijkspicy@outlook.com <dijkspicy@outlook.com>
2018-07-07 01:01:06 +0800

    rename package

--- server/src/main/java/dijkspicy/ms/server/dispatch/connector/ConnectorHandler.java
+++ server/src/main/java/dijkspicy/ms/server/dispatch/connector/ConnectorHandler.java
@@ -3,23 +3,15 @@ package dijkspicy.ms.server.dispatch.connector;
 import dijkspicy.ms.server.dispatch.BaseHandler;
 import dijkspicy.ms.server.dispatch.HttpContext;
 import dijkspicy.ms.server.dispatch.ServiceException;
-import org.apache.calcite.avatica.AvaticaConnection;
-import org.apache.calcite.avatica.Meta;
-import org.apache.calcite.avatica.remote.JsonService;
-import org.apache.calcite.avatica.remote.LocalService;
+import org.apache.calcite.avatica.remote.LocalJsonService;
 import org.apache.calcite.avatica.remote.Service;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.util.Properties;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -74,44 +66,23 @@ public class ConnectorHandler extends BaseHandler<String> {
 
     @Override
     protected String doMainLogic(HttpContext context) throws ServiceException {
-        AvaticaConnection connection = (AvaticaConnection) Connector.SOLE.get(this.type);
-
-        try (PreparedStatement statement = connection.prepareStatement("select * from DEPTS")) {
-            ResultSet resultSet = statement.executeQuery();
-            try {
-                final ResultSetMetaData metaData = resultSet.getMetaData();
-                final int columnCount = metaData.getColumnCount();
-                PrintStream out = System.out;
-                while (resultSet.next()) {
-                    for (int i = 1; ; i++) {
-                        out.print(resultSet.getString(i));
-                        if (i < columnCount) {
-                            out.print(", ");
-                        } else {
-                            out.println();
-                            break;
-                        }
-                    }
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
         try {
-            Field field = AvaticaConnection.class.getDeclaredField("meta");
-            field.setAccessible(true);
-            Meta meta = (Meta) field.get(connection);
-            Service service = new LocalService(meta);
-            Service.Request request = JsonService.MAPPER.readValue(this.data, Service.Request.class);
-            Object response = ACCEPT_METHOD.invoke(request, service);
-            return JsonService.MAPPER.writeValueAsString(response);
-        } catch (IOException e) {
-            throw new ServiceException("Got an illegal request message", e);
+            LocalJsonService service = new LocalJsonService(MockConnector.TQL.connector.getService());
+            return service.apply(this.data);
         } catch (Exception e) {
             throw new ServiceException("Failed to accept service: " + e.getMessage(), e);
+        }
+    }
+
+    private enum MockConnector {
+        TQL;
+
+        public final CalciteConnector connector;
+
+        MockConnector() {
+            Properties info = new Properties();
+            info.setProperty("model", "server/src/main/data/model.json");
+            connector = new CalciteConnector(info);
         }
     }
 }
--- server/src/test/java/dijkspicy/ms/server/ApplicationTest.java
+++ server/src/test/java/dijkspicy/ms/server/ApplicationTest.java
@@ -3,7 +3,6 @@ package dijkspicy.ms.server;
 import org.junit.Test;
 
 import java.sql.*;
-import java.util.Properties;
 import java.util.StringJoiner;
 
 /**
@@ -15,7 +14,7 @@ import java.util.StringJoiner;
 public class ApplicationTest {
 
     @Test
-    public void testSend() throws ClassNotFoundException, SQLException {
+    public void jdbc() throws ClassNotFoundException, SQLException {
         Class.forName(org.apache.calcite.avatica.remote.Driver.class.getName());
 
         Connection connection = DriverManager.getConnection("jdbc:avatica:remote:url=http://localhost:8443/ms/jdbc/model");
@@ -25,36 +24,13 @@ public class ApplicationTest {
              ResultSet resultSet = statement.executeQuery()) {
             ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
-            StringJoiner joiner = new StringJoiner("\r\n");
             while (resultSet.next()) {  //while控制行数
-                StringBuilder sb = new StringBuilder();
+                StringJoiner sb = new StringJoiner("\t");
                 for (int i = 1; i <= columnCount; i++) {//for循环控制列数
-                    sb.append(resultSet.getObject(i))
-                            .append("\t");
+                    sb.add(String.valueOf(resultSet.getObject(i)));
                 }
-                joiner.add(sb.toString());
+                System.out.println(sb);
             }
-
-            System.out.println("---------------------------------------------");
-            System.out.println(joiner);
-            System.out.println("---------------------------------------------");
-        }
-    }
-
-    @Test
-    public void testEnv() throws ClassNotFoundException, SQLException {
-        Class.forName(org.apache.calcite.avatica.remote.Driver.class.getName());
-
-        Properties info = new Properties();
-        info.setProperty("truststore", "E:\\project\\github\\queeng\\.any\\ares\\trust.jks");
-        info.setProperty("truststore_password", "Changeme_123");
-        String url = "https://10.180.42.209:32018/rest/odae/v1/queryengine/aql/query";
-        Connection connection = DriverManager.getConnection("jdbc:avatica:remote:url=" + url, info);
-
-        String sql = "select *";
-        try (PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
-            System.out.println(resultSet);
         }
     }
 }
